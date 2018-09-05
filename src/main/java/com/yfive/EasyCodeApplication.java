728bf2e8b8a65912696f994ca9ce4b8dd0d909a8
huangyufei <834542418@qq.com>
2018-09-05 19:50:38 +0800

    构建项目:配置项目基础信息:@yfive

---
+++ src/main/java/com/yfive/EasyCodeApplication.java
@@ -0,0 +1,12 @@
+package com.yfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCodeApplication.class, args);
+	}
+}

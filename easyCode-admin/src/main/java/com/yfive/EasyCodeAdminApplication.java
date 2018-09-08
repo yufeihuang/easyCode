bf4dfdea4545fe184515a44e6b265956ef167ebb
huangyufei <834542418@qq.com>
2018-09-08 16:37:25 +0800

    # build:构建项目:@黄宇飞
    # 添加子项目
    #   -admin后台管理模块
    
    # remark:。。。
    # todo：。。。
    # another：。。。

---
+++ easyCode-admin/src/main/java/com/yfive/EasyCodeAdminApplication.java
@@ -0,0 +1,12 @@
+package com.yfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCodeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCodeAdminApplication.class, args);
+	}
+}
--- easyCode-parent/easyCode-common/src/main/java/com/yfive/EasyCodeCommonApplication.java
+++ easyCode-parent/easyCode-common/src/main/java/com/yfive/EasyCodeCommonApplication.java
@@ -1,4 +1,4 @@
-package com.yFive;
+package com.yfive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
